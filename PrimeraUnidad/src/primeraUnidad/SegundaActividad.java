80707ecfb9ed9f5c47c3f1b35044ab39f1254f45
Jose Manuel Vilomar <jvilomar@gmail.com>
2020-11-15 10:07:09 -0400

    Refactored code to bono method

--- PrimeraUnidad/src/primeraUnidad/SegundaActividad.java
+++ PrimeraUnidad/src/primeraUnidad/SegundaActividad.java
@@ -24,23 +24,9 @@ public class SegundaActividad {
 	 * Calcula el Monto del Bono a recibir dependiendo de los años laborados.
 	 * */
 	private static Float CalcularBono(int tiempoLaborado) {
-		Float result = 0.00f;
-
 		if (tiempoLaborado > 5) {  return 1000.00f; }; 
 		
-		if(tiempoLaborado == 1 ) { 
-			result = 100f; 
-		} else if (tiempoLaborado == 2  ) {
-			result = 200f;
-		} else if (tiempoLaborado == 3) {
-			result = 300f;
-		} else if(tiempoLaborado == 4) {
-			result = 400f;
-		}else if(tiempoLaborado == 5) {
-			result = 500f;
-		}
-		
-		return result;
+		return tiempoLaborado * 100f;
 	}
 
 }
